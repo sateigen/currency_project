74990fc6a948d76314ed4d43e7a013fa3323c10e
sateigen <shannon.teigen@gmail.com>
2016-07-18 14:21:20 -0400

    Two given amounts can be equal.

--- currency_test.py
+++ currency_test.py
@@ -5,3 +5,9 @@ def test_currency_has_amount_and_currency_code():
 
     assert one_dollar.amount == 1
     assert one_dollar.currency_code == 'USD'
+
+def test_currency_can_be_equal():
+    currency1 = Currency(50, 'USD')
+    currency2 = Currency(50, 'USD')
+
+    assert currency1 == currency2
