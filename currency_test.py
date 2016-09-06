212899ff361007f86f73ae72d8c32fc829d4c405
sateigen <shannon.teigen@gmail.com>
2016-09-06 14:20:22 -0400

    Fixed variable names in test_currency_items_integer and test_replace_symbol_with_currency_code

--- currency_test.py
+++ currency_test.py
@@ -1,6 +1,7 @@
 from currency import Currency, DifferentCurrencyCodeError
 from nose.tools import assert_raises
 
+
 def test_currency_has_amount_and_currency_code():
     one_dollar = Currency(1, 'USD')
 
@@ -49,6 +50,8 @@ def test_get_error_when_using_different_currency_codes():
 
     assert_raises(DifferentCurrencyCodeError, Currency.__add__, currency1, currency2)
     assert_raises(DifferentCurrencyCodeError, Currency.__sub__, currency1, currency2)
+# with assert_raises(DifferentCurrencyCodeError):
+    # currency1 - currency2
 
 
 def test_currency_times_integer():
@@ -57,7 +60,7 @@ def test_currency_times_integer():
     number2 = 10.0
 
     assert currency1 * number == (50, 'USD')
-    assert currency1 * number == (50.0, 'USD')
+    assert currency1 * number2 == (50.0, 'USD')
 
 
 def test_replace_symbol_with_currency_code():
@@ -66,5 +69,5 @@ def test_replace_symbol_with_currency_code():
     currency3 = '¥5.65'
 
     assert Currency(Currency.take_symbol_off_amount(currency1), Currency.assign_currency_code_for_symbol(currency1)) == Currency(5.65, 'USD')
-    assert Currency(Currency.take_symbol_off_amount(currency1), Currency.assign_currency_code_for_symbol(currency1)) == Currency(5.65, 'EUR')
-    assert Currency(Currency.take_symbol_off_amount(currency1), Currency.assign_currency_code_for_symbol(currency1)) == Currency(5.65, 'JPY')
+    assert Currency(Currency.take_symbol_off_amount(currency2), Currency.assign_currency_code_for_symbol(currency2)) == Currency(5.65, 'EUR')
+    assert Currency(Currency.take_symbol_off_amount(currency3), Currency.assign_currency_code_for_symbol(currency3)) == Currency(5.65, 'JPY')
